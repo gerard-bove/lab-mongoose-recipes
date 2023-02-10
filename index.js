200d9a7353ab66bc33fe94c3a950fb8d1802e9a8
Gerard Bove <gerardbovetous@gmail.com>
2023-02-10 17:25:45 +0100

    iteration 4 done

--- index.js
+++ index.js
@@ -5,10 +5,31 @@ const Recipe = require('./models/Recipe.model');
 // Import of the data from './data.json'
 const data = require('./data');
 
-const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
+const MONGODB_URI = 'mongodb://127.0.0.1:27017/recipi-app';
 
 // Connection to the database "recipe-app"
+const receta = {
+  "title": "Asian Chicken",
+  "level": "Amateur Chef",
+  "ingredients": [
+    "1/2 cup rice vinegar",
+    "5 tablespoons honey",
+    "1/3 cup soy sauce (such as Silver Swan®)",
+    "1/4 cup Asian (toasted) sesame oil",
+    "3 tablespoons Asian chili garlic sauce",
+    "3 tablespoons minced garlic",
+    "salt to taste",
+    "8 skinless, boneless chicken thighs"
+  ],
+  "cuisine": "Asian",
+  "dishType": "main_course",
+  "image": "https://images.media-allrecipes.com/userphotos/720x405/815964.jpg",
+  "duration": 40,
+  "creator": "Chef LePapu"
+}
+
 mongoose
+  .set('strictQuery', false)
   .connect(MONGODB_URI)
   .then(x => {
     console.log(`Connected to the database: "${x.connection.name}"`);
@@ -17,7 +38,23 @@ mongoose
   })
   .then(() => {
     // Run your code here, after you have insured that the connection was made
+    return Recipe.create(receta);
   })
+  .then(() => {
+    console.log(receta.title)
+  })
+  .then(() => {
+      return Recipe.insertMany(data)
+  })
+  .then(() => {
+    data.forEach(element => {
+      console.log(element.title)
+    });
+  })
+  .then(() => {
+    return Recipe.findOneAndUpdate({title: "Rigatoni alla Genovese"}, {duration: 100})
+  })
+
   .catch(error => {
     console.error('Error connecting to the database', error);
   });
